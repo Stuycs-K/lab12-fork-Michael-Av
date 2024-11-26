24d23a9db99b1b9ccb0d7b0e99110d8fcb8d67c7
Michael Avrahami <mavrahami50@stuy.edu>
2024-11-26 14:44:03 -0500

    Wrote parent and child code

--- fork.c
+++ fork.c
@@ -4,8 +4,59 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <fcntl.h>
+
+void err(){
+  perror("fork failed");
+  exit(1);
+}
+
+int child(){
+  printf("%d ", getpid());
+  long int randNum;
+  int fd = open("/dev/urandom", O_RDONLY);
+  read(fd, &randNum, 4);
+  randNum = randNum % 5 + 1;
+  printf("%ldsec\n", randNum);
+
+  sleep(randNum);
+
+  printf("%d finished after %ldsec\n", getpid(), randNum);
+
+  return randNum;
+}
+
+int parent(int child1, int child2){
+  //printf("child1: %d, child2: %d\n", child1, child2);
+  int status;
+  int childWhoReturned;
+  childWhoReturned = wait(&status);
+
+  int childSleep = WEXITSTATUS(status);
+
+  printf("Main process %d is done. Child %d slept for %dsec\n", getpid(), childWhoReturned, childSleep);
+  return 0;
+}
 
 int main(){
-  pid_t p;
-  printf("%d\n", p);
+  pid_t p1, p2;
+  printf("%d about to create 2 child processes\n", getpid());
+
+  p1 = fork();
+  if (p1 < 0) err();
+
+  else if (p1 == 0){
+    return child();
+  }
+
+  if (p1 > 0){
+    p2 = fork();
+    if (p2 == 0){
+      return child();
+    }
+
+    parent(p1, p2);
+  }
+
+  return 0;
 }
